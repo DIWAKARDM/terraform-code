eeef8be856a5ce2042397860e567673d0dc1bc26
Diwakar Mishra <diwakar.dm@gmail.com>
2022-08-13 22:47:10 +0530

    Adding first code

---
+++ variable.tf
@@ -0,0 +1,13 @@
+variable "aws_region" {
+        default = ""
+
+}
+
+variable "vpc_cidr" {
+        default = ""
+}
+
+variable "envi" {
+        default = ""
+}
+
---
+++ vpc.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "${var.aws_region}"
+  #shared_credentials_files = "$HOME/.aws/credentials"
+  profile                 = "default"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "${var.vpc_cidr}"
+
+  tags = {
+    Name = "${var.envi}_${var.aws_region}"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-backend-testt"
+    key    = "terraform.state"
+    region = "ap-south-1"
+  }
+} 
